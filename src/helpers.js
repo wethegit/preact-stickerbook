d7fa2ef3e422bc5640fcb8d3c0554c78bf657498
Marlon Marcello <marlon.marcello@gmail.com>
2021-02-22 17:31:36 -0800

    Removed old exporter

--- src/helpers.js
+++ src/helpers.js
@@ -7,50 +7,6 @@ export function classnames(namesArray) {
     .trim();
 }
 
-export function stickerbookToJson({ background, frame, stickers, foreground }) {
-  if (!background && !stickers && !foreground && !frame) {
-    console.log("No data provided");
-    return;
-  }
-
-  const { image, alt, ...bg } = background;
-
-  const string = JSON.stringify(
-    {
-      background: bg,
-      stickers: stickers.map((sticker) => {
-        const { image, key, position, alt, ...props } = sticker;
-
-        return {
-          ...props,
-          position: {
-            x: position?.x,
-            y: position?.y,
-          },
-        };
-      }),
-    },
-    null,
-    2
-  );
-
-  console.log(
-    "------------------------------------------\n",
-    `${new Date().toLocaleString("en-CA")}\n`,
-    "------------------------------------------\n",
-    string,
-    "\n------------------------------------------"
-  );
-
-  navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
-    if (result.state == "granted" || result.state == "prompt") {
-      navigator.clipboard.writeText(string).then(() => {
-        console.log(`📋 Copied to clipboard`);
-      });
-    }
-  });
-}
-
 // Helpers function to create images from paths
 export function loadUrlAsImage(item) {
   return new Promise(function (resolve, reject) {
