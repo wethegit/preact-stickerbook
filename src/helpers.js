f20715e670fa2e909e0a06514759f197752f1c4c
Marlon Marcello <marlon.marcello@gmail.com>
2021-02-22 21:45:46 -0800

    Removed hooks in favour of simpler helpers

--- src/helpers.js
+++ src/helpers.js
@@ -307,3 +307,23 @@ export function deleteSticker(stickers, index) {
       return item;
     });
 }
+
+export function patchSticker({ stickers, prop, value, index }) {
+  if (!stickers || !stickers instanceof Array || stickers.length <= 0)
+    throw Error("`stickers` array is empty");
+
+  if (typeof index !== "number" || !stickers[index])
+    throw Error("`index` needs to be a valid `stickers` array index");
+
+  const PROPS = ["position", "scale", "rotation"];
+
+  if (!PROPS.includes(format))
+    throw Error(`Invalid 'prop'. 'prop' must be one of: ${PROPS.join(",")}`);
+
+  if (stickers[index][prop] && stickers[index][prop] === value) return stickers;
+
+  return stickers.map((item, i) => {
+    if (i === index) item[prop] = value;
+    return item;
+  });
+}
