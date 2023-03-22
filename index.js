fb38ee344be814a685b642d2934a9d32f9e0ad88
Liam Egan <liam@wethecollective.com>
2023-03-22 10:36:58 -0700

    Adding bundler

--- index.js
+++
@@ -1,6 +0,0 @@
-export { classnames, loadUrlAsImage, coverCanvas, exportStickerbook, reorderSticker, addSticker, deleteSticker, patchSticker } from "./lib/helpers";
-export { renderSticker } from "./lib/renderSticker.js"
-export { Sticker } from "./lib/sticker.js"
-export { Stickerbook } from "./lib/sticker.js"
-
-
---
+++ src/index.js
@@ -0,0 +1,6 @@
+export { classnames, loadUrlAsImage, coverCanvas, exportStickerbook, reorderSticker, addSticker, deleteSticker, patchSticker } from "./helpers";
+export { renderSticker } from "./renderSticker"
+export { Sticker } from "./sticker"
+export { Stickerbook } from "./sticker"
+
+
