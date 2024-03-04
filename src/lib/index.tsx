1837ed741138cedd9ab0226f8068d596a2a2083d
Marlon Marcello <marlon.marcello@gmail.com>
2024-03-04 11:18:28 -0800

    fix: granular export from index

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -2,6 +2,29 @@ export { Stickerbook } from "./stickerbook"
 export { Sticker } from "./sticker"
 export { useStickerbook } from "./use-stickerbook"
 
-export * from "./helpers"
+export {
+  addSticker,
+  deleteSticker,
+  exportStickerbook,
+  patchSticker,
+  reorderSticker,
+} from "./helpers"
 
-export type * from "./types"
+export type {
+  StickerItem,
+  BackgroundItem,
+  ForegroundItem,
+  Frame,
+  StickerbookProps,
+  StickerbookContextProps,
+  ExportFormat,
+  OrderDirection,
+  OnDeleteHandler,
+  OnReorderHandler,
+  OnPositionHandler,
+  OnScaleHandler,
+  OnRotateHandler,
+  OnAddStickerHandler,
+  StickerProps,
+  UseStickerbookProps,
+} from "./types"
